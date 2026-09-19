eaa928ea8cdd0ae07a27e271cc6e74deb8fca4ce
Umang Srivastava <btecg10271.23@bitmesra.ac.in>
2026-09-20 01:09:30 +0530

    Test

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -4,15 +4,15 @@ import { assets } from '../assets/assets'
 
 function Hero() {
 
-    const naviagte = useNavigate()
+  const naviagte = useNavigate()
 
   return (
     <div className='px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen'>
-      
+
       <div className='text-center mb-6'>
-        <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]'>Create amazing content <br/> with <span className='text-primary'>AI tools</span></h1>
+        <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]'>Create amazing content <br /> with <span className='text-primary'>AI tools</span></h1>
         <p className='mt-4 max-w-xs sm:max-w-lg 2xl:max-w-xl m-auto max-sm:text-xs tex-gray-600'>Transform your content creation with our suite of premium AI tools.
-            Write articles, generate images and enhance your workflow.
+          Write articles, generate images and enhance your workflow.
         </p>
       </div>
       <div className='flex flex-wrap justify-center gap-4 text-sm max-sm:text-xs'>
